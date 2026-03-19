47a83f9e9f31729da6544450b49be5e580cc04d5
sahadathussain12 <sahadathussain872856@gmail.com>
2026-03-19 20:19:57 +0600

    feat: implement login functionality with username/password validation and redirect on success

---
+++ js/loging.js
@@ -0,0 +1,22 @@
+console.log("hello");
+
+
+
+
+document.getElementById('loging').addEventListener('click',function(){
+    const userInput = document.getElementById('ussrname')
+    const userName = userInput.value
+    console.log(userName);
+
+    const inputPass = document.getElementById('pass')
+    const password = inputPass.value
+    console.log(password);
+
+    if(userName==='admin'&& password === 'admin123'){
+        alert('loging success')
+        window.location.assign("/home.html")
+    }
+    else{
+        alert('loging faled')
+    }
+})
